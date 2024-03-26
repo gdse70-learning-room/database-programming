fd284d9ab9b59b376799f361763f2fa65fd6bad5
danujav <danujagreru@gmail.com>
2024-03-26 15:57:38 +0530

    complete login navigation

--- _05-supermarket/src/main/java/lk/ijse/supermarket/controller/LoginFormController.java
+++ _05-supermarket/src/main/java/lk/ijse/supermarket/controller/LoginFormController.java
@@ -8,15 +8,49 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import lk.ijse.supermarket.db.DbConnection;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class LoginFormController {
-    public TextField txtUserName;
+    public TextField txtUserId;
     public TextField txtPassword;
     public AnchorPane rootNode;
 
     public void btnLoginOnAction(ActionEvent actionEvent) throws IOException {
+        String userId = txtUserId.getText();
+        String pw = txtPassword.getText();
 
+        try {
+            checkCredential(userId, pw);
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, "OOPS! something went wrong").show();
+        }
+    }
+
+    private void checkCredential(String userId, String pw) throws SQLException {
+        String sql = "SELECT user_id, password FROM users WHERE user_id = ?";
+
+        Connection connection = DbConnection.getInstance().getConnection();
+        PreparedStatement pstm = connection.prepareStatement(sql);
+        pstm.setObject(1, userId);
+
+        ResultSet resultSet = pstm.executeQuery();
+        if(resultSet.next()) {
+            String dbPw = resultSet.getString(2);
+
+            if(dbPw.equals(pw)) {
+                System.out.println("Navigate to the dashboard");
+            } else {
+                new Alert(Alert.AlertType.ERROR, "Password is incorrect!").show();
+            }
+        } else {
+            new Alert(Alert.AlertType.INFORMATION, "user id not found!").show();
+        }
     }
 
     public void linkRegistrationOnAction(ActionEvent actionEvent) throws IOException {
