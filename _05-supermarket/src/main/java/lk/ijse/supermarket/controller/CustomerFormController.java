e63036dd3b493e661ebf705b5bfa71261db2c849
danujav <danujagreru@gmail.com>
2024-04-02 09:55:23 +0530

    customer basic crud done

--- _05-supermarket/src/main/java/lk/ijse/supermarket/controller/CustomerFormController.java
+++ _05-supermarket/src/main/java/lk/ijse/supermarket/controller/CustomerFormController.java
@@ -4,13 +4,20 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import lk.ijse.supermarket.db.DbConnection;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class CustomerFormController {
     @FXML
     private TableColumn<?, ?> colAddress;
@@ -44,26 +51,116 @@ public class CustomerFormController {
 
     @FXML
     void btnSaveOnAction(ActionEvent event) {
+        String id = txtId.getText();
+        String name = txtName.getText();
+        String address = txtAddress.getText();
+        String tel = txtTel.getText();
 
+        String sql = "INSERT INTO customers VALUES(?, ?, ?, ?)";
+
+        try {
+            Connection connection = DbConnection.getInstance().getConnection();
+            PreparedStatement pstm = connection.prepareStatement(sql);
+
+            pstm.setObject(1, id);
+            pstm.setObject(2, name);
+            pstm.setObject(3, address);
+            pstm.setObject(4, tel);
+
+            boolean isSaved = pstm.executeUpdate() > 0;
+            if(isSaved) {
+                new Alert(Alert.AlertType.CONFIRMATION, "customer saved!").show();
+                clearFields();
+            }
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
+    }
+
+    private void clearFields() {
+        txtId.setText("");
+        txtName.setText("");
+        txtAddress.setText("");
+        txtTel.setText("");
     }
 
     @FXML
     void btnUpdateOnAction(ActionEvent event) {
+        String id = txtId.getText();
+        String name = txtName.getText();
+        String address = txtAddress.getText();
+        String tel = txtTel.getText();
 
+        String sql = "UPDATE customers SET name = ?, address = ?, tel = ? WHERE id = ?";
+
+        try {
+            PreparedStatement pstm = DbConnection.getInstance().getConnection()
+                    .prepareStatement(sql);
+
+            pstm.setObject(1, name);
+            pstm.setObject(2, address);
+            pstm.setObject(3, tel);
+            pstm.setObject(4, id);
+
+            if(pstm.executeUpdate() > 0) {
+                new Alert(Alert.AlertType.CONFIRMATION, "customer updated!").show();
+                clearFields();
+            }
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
     }
 
     @FXML
     void txtSearchOnAction(ActionEvent event) {
+        String id = txtId.getText();
 
+        String sql = "SELECT * FROM customers WHERE id = ?";
+        try {
+            Connection connection = DbConnection.getInstance().getConnection();
+            PreparedStatement pstm = connection.prepareStatement(sql);
+            pstm.setObject(1, id);
+
+            ResultSet resultSet = pstm.executeQuery();
+            if(resultSet.next()) {
+                String name = resultSet.getString(2);
+                String address = resultSet.getString(3);
+                String tel = resultSet.getString(4);
+
+                txtName.setText(name);
+                txtAddress.setText(address);
+                txtTel.setText(tel);
+            } else {
+                new Alert(Alert.AlertType.INFORMATION, "customer id can't be find!").show();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
     void btnDeleteOnAction(ActionEvent event) {
+        String id = txtId.getText();
 
+        String sql = "DELETE FROM customers WHERE id = ?";
+
+        try {
+            PreparedStatement pstm = DbConnection.getInstance().getConnection()
+                    .prepareStatement(sql);
+            pstm.setObject(1, id);
+
+            if(pstm.executeUpdate() > 0) {
+                new Alert(Alert.AlertType.CONFIRMATION, "customer deleted!").show();
+                clearFields();
+            }
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
     }
 
     @FXML
     void btnClearOnAction(ActionEvent event) {
+        clearFields();
     }
 
     @FXML
