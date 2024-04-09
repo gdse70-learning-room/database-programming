be1126ea716b211744fdcee7ec939990ddac819c
danujav <danujagreru@gmail.com>
2024-04-09 09:50:28 +0530

    add icons

--- _06-supermarket-mvc/src/main/java/lk/ijse/supermarket/Launcher.java
+++ _06-supermarket-mvc/src/main/java/lk/ijse/supermarket/Launcher.java
@@ -15,7 +15,7 @@ public class Launcher extends Application {
         stage
                 .setScene(new Scene(FXMLLoader
                         .load(this.getClass()
-                                    .getResource("/view/login_form.fxml"))));
+                                    .getResource("/view/placeorder_form.fxml"))));
 
         stage.setTitle("Login Form");
         stage.centerOnScreen();
